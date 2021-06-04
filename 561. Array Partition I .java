fe0f0806303cfee8d98094ca1fc9ca5b05767dc5
Benjamin Chadwick <73488828+bchadwic@users.noreply.github.com>
2021-06-04 02:16:13 -0700

    Create 561. Array Partition I .java

---
+++ 561. Array Partition I .java
@@ -0,0 +1,11 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        
+        int result = 0;
+        for(int i = 0; i < nums.length; i += 2)
+            result += Math.min(nums[i], nums[i+1]);
+        
+        return result;
+    }
+}
