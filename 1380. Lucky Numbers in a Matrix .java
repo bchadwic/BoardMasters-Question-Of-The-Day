a730717f0973f6aa0f8f922fb3ec2b867b4b1f59
Benjamin Chadwick <73488828+bchadwic@users.noreply.github.com>
2021-06-09 10:49:50 -0700

    Create 1380. Lucky Numbers in a Matrix .java

---
+++ 1380. Lucky Numbers in a Matrix .java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<Integer> luckyNumbers (int[][] matrix) {
+        
+        Set<Integer> rowMins = new HashSet<>();
+        Set<Integer> colMaxs = new HashSet<>();
+        
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        
+        for(int row = 0; row < matrix.length; row++){ // traverse the rows
+            for(int col = 0; col < matrix[row].length; col++){
+                min = Math.min(matrix[row][col], min);
+            }
+            rowMins.add(min);
+            min = Integer.MAX_VALUE;
+        }
+        
+        for(int col = 0; col < matrix[0].length; col++){ // traverse the cols
+            for(int row = 0; row < matrix.length; row++){
+                max = Math.max(matrix[row][col], max);
+            }
+            colMaxs.add(max);
+            max = Integer.MIN_VALUE;
+        }
+        
+        rowMins.retainAll(colMaxs); // get the intersection of the two sets
+        return new ArrayList<Integer>(rowMins);
+    }
+}
