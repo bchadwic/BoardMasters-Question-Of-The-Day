44c5ecafa89c8b88a48dda1a2643fcfe9aeb237e
Benjamin Chadwick <73488828+bchadwic@users.noreply.github.com>
2021-07-03 14:15:12 -0700

    Update 118. Pascal's Triangle .java

--- 118. Pascal's Triangle .java
+++ 118. Pascal's Triangle .java
@@ -7,17 +7,14 @@ class Solution {
             
             List<Integer> row = new ArrayList<Integer>(i + 1);
             
-            while(row.size() < i + 1) row.add(0);
-            
-            row.set(0, 1);
-            row.set(i, 1);
+            while(row.size() < i + 1) row.add(1);
             
             for(int j = 1; j < i; j++){ // filling in the inner part of the rows
                 row.set(
                        j, 
                        pascal.get(i - 1).get(j) +
                        pascal.get(i - 1).get(j - 1) 
-                     );
+                       );
             }
             
             pascal.add(row); // add a new row
