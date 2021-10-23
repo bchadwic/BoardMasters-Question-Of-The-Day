45c823b885275b191899e822a2fef4501cd08a78
bchadwic <bchadwick@mail.greenriver.edu>
2021-10-23 14:36:21 -0700

    Created by bchadwic

---
+++ c/1816. Truncate Sentence .c
@@ -0,0 +1,11 @@
+char * truncateSentence(char * s, int k){
+    int words = 0;
+    char * p = s;
+    while(*p != '\0' && words < k){
+       ++p;
+       if(*p == ' ') 
+           words++; 
+    } 
+    *p = '\0';
+    return s;
+}
