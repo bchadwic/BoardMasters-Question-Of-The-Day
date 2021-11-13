6af16e4342f5b4fc6236523712d594dce49931c4
Joseph <jcigama77@yahoo.com>
2021-11-13 12:33:04 -0800

    Solution for LeetCode problem #1323: Joseph Igama

---
+++ java/Solution_1323.java
@@ -0,0 +1,29 @@
+//
+
+public class Solution_1323
+{
+    public static int maximum69Number (int num)
+    {
+        StringBuilder numString = new StringBuilder(Integer.toString(num));
+
+        for (int i = 0; i < numString.length(); i++)
+        {
+            if (numString.charAt(i) != '9')
+            {
+                numString.setCharAt(i, '9');
+
+                if (Integer.parseInt(String.valueOf(numString)) > num)
+                {
+                    return Integer.parseInt(String.valueOf(numString));
+                }
+            }
+        }
+
+        return num;
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(maximum69Number(9669));
+    }
+}
