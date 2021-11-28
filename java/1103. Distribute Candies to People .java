29070c0250b7ea3f91db04076c8aff5504a9caec
jcigama <72955905+jcigama@users.noreply.github.com>
2021-11-28 12:20:29 -0800

    Solution for LeetCode problem #1323: Joseph Igama (#38)
    * Solution for problem #211
    
    * merging prev approach with new
    
    * Solution for LeetCode problem #1773: Joseph Igama
    
    * Rename Solution_1773.java to 1773. Count Items Matching a Rule
    
    * Rename 1773. Count Items Matching a Rule to 1773. Count Items Matching a Rule .java
    
    * Solution for LeetCode problem #1103: Joseph Igama
    
    * Rename 1103.  to 1103. Distribute Candies to People
    
    * Solution for LeetCode problem #1103: Joseph Igama
    
    * Solution for LeetCode problem #1323: Joseph Igama
    
    * rename extension
    
    * Solution for LeetCode problem #693: Joseph Igama
    
    * Renamed files for consistency
    
    * Solution for problem #747
    
    * removing solutions with incorrectly formatted names
    
    Co-authored-by: bchadwic <bchadwick@mail.greenriver.edu>
    Co-authored-by: Benjamin Chadwick <73488828+bchadwic@users.noreply.github.com>
    Co-authored-by: bchadwic <benchadwick87@gmail.com>

---
+++ java/1103. Distribute Candies to People .java
@@ -0,0 +1,35 @@
+// Joseph Igama
+// Solution for problem #1103
+
+public class Solution
+{
+    public int[] distributeCandies(int candies, int num_people) {
+        int[] candiesToPeople = new int[num_people];
+        int candyDistribution = 1;
+
+        while (candies > 0)
+        {
+            for (int i = 0; i < candiesToPeople.length; i++)
+            {
+                if (candyDistribution > candies)
+                {
+                    candiesToPeople[i] = candiesToPeople[i] + candies;
+                }
+                else
+                {
+                    candiesToPeople[i] = candiesToPeople[i] + candyDistribution;
+                }
+
+                candies = candies - candyDistribution;
+                candyDistribution++;
+
+                if(candies <= 0)
+                {
+                    break;
+                }
+            }
+        }
+
+        return candiesToPeople;
+    }
+}
